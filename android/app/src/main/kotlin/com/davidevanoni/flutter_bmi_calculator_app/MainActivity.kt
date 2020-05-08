47c57952013874f4d9018b74893a5ab2f1eb65be
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-08 12:10:48 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/davidevanoni/flutter_bmi_calculator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.davidevanoni.flutter_bmi_calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Overwrite the dark theme instead of writing a new Theme
+      /*
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      */
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
